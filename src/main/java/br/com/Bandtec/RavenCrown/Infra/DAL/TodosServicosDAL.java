98dceb104f80513f970a98b07fe1ec0049c4fbd8
Matheus Vicente <mvicente@outlook.com.br>
2019-05-17 18:34:35 -0300

    asserto da classe servicoDAL

--- src/main/java/br/com/Bandtec/RavenCrown/Infra/DAL/TodosServicosDAL.java
+++ src/main/java/br/com/Bandtec/RavenCrown/Infra/DAL/TodosServicosDAL.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface TodosServicosDAL extends JpaRepository<ServicoEntity, Integer> {
 
-    @Query("from servicoEntity where id_prestador = ?1")
+    @Query("from ServicoEntity where id_prestador = ?1")
     List<ServicoEntity> GetAllServicesByUserId(int id);
 
 }
